ac4ee0052d46ebe12f88cc6c64b286430f54eb28
Johan Herrera Araya <johanherreraaraya@gmail.com>
2019-11-23 01:25:20 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Nums()
+        {
+            int min = 1;
+            int max = 15;
+            ViewBag.min = "The min num is: " + min.ToString();
+            ViewBag.max = "The max num is: " + max.ToString();
+            ViewBag.num = "The random number is: " + RanNum(min, max).ToString();
+            return View();
+        }
+
+        public int RanNum(int min, int max)
+        {
+            return new Random().Next(min, max);
+        }
+    }
+}
